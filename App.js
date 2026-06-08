3da00b8b773904503c250aaf5c58c89c45c0bee0
Nicolas Gutierrez <ngutierrezj@unicartagena.edu.co>
2026-06-07 23:09:22 -0500

    Clase #28 terminada

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 const btnMensaje = document.getElementById('btnMensaje');
 const MensajeClase = document.getElementById('MensajeClase');
-
+const botonesPerfil = document.querySelectorAll('.btnPerfil'); 
+const detallePerfil = document.getElementById('detallePerfil');
 let contadorDeClicks = 0;
-btnMensaje.addEventListener('click', () => {
-    MensajeClase.textContent = 'Hola, esta es una alerta de prueba';
-    
-    // MensajeClase.textContent = 'Eltexto que quisimos mostrar';
+
+        btnMensaje.addEventListener('click', () => {
     contadorDeClicks++;
-    MensajeClase.textContent = `Has hecho clic ${contadorDeClicks} veces`;
+    MensajeClase.textContent = `Hola, esta es una alerta de prueba. Has hecho clic ${contadorDeClicks} veces`;
+});
+
+
+botonesPerfil.forEach((boton) => {
+    boton.addEventListener('click', () => {
+        const perfil = boton.getAttribute('data-perfil');
+        detallePerfil.textContent = `Información detallada del perfil: ${perfil}`;
+    });
 });
